2cf914259dc01a73311eb84a511f82466020d45e
Gabriel <hugo.silva@ccc.ufcg.edu.br>
2017-11-30 00:31:15 -0300

    Adiciona variáveis sendo medidas

--- medicao/medicao.sh
+++ medicao/medicao.sh
@@ -3,7 +3,7 @@
 for i in {0..10}
 do
     echo $(date +%s%N) ",inicio ,"$i >> requestlogput250.txt
-    curl -X PUT -d '{"status":"Em Preparo"}' https://sistemaalocacao.firebaseapp.com/classes >> dblogput250.txt
+    curl -w %{time_namelookup},%{time_connect},%{time_starttransfer},%{time_total}\\n -s https://sistemaalocacao.firebaseapp.com/classes/ -o /dev/null  >> dblogput250.txt
     echo $(date +%s%N) ", fim, "$i >> requestlogput250.txt
     sleep .1
 done
